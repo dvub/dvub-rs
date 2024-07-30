d67db9635aa3ceb48db8a56bfa11e02de31d049f
dvub <dvubdevs@gmail.com>
2024-07-30 01:47:58 -0700

    some more epic progress

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use std::{fs::read_dir, path::Path};
+use std::{
+    fs::{read_dir, read_to_string},
+    path::Path,
+};
 
 use askama::Template;
-use axum::{response::Html, routing::get, Router};
+use axum::{routing::get, Router};
 use notify::{RecursiveMode, Watcher};
+use scraper::{Html, Selector};
 use tokio::net::TcpListener;
 
 #[tokio::main]
@@ -41,7 +45,8 @@ struct PostsTemplate {
 }
 
 struct Post {
-    name: String,
+    title: String,
+    description: String,
 }
 
 async fn root() -> HomeTemplate {
@@ -49,27 +54,57 @@ async fn root() -> HomeTemplate {
 }
 
 async fn posts() -> PostsTemplate {
-    PostsTemplate { posts: Vec::new() }
+    let posts = get_posts(Path::new("./templates/posts"));
+
+    PostsTemplate { posts }
+}
+
+fn get_posts(path: &Path) -> Vec<Post> {
+    let posts_dir = read_dir(path).unwrap();
+
+    let mut posts_vec = Vec::new();
+
+    for entry in posts_dir {
+        let path = entry.unwrap().path();
+        let str = read_to_string(path).unwrap();
+
+        let html = Html::parse_fragment(&str);
+
+        let title_selector = Selector::parse("title").unwrap();
+        let meta_selector = Selector::parse("meta").unwrap();
+
+        let mut title_results = html.select(&title_selector);
+        let meta_results = html.select(&meta_selector);
+
+        let title = title_results.nth(0).unwrap().inner_html();
+
+        let mut description = "";
+
+        for n in meta_results {
+            if n.attr("name") == Some("description") {
+                description = n.attr("content").unwrap();
+            }
+        }
+        posts_vec.push(Post {
+            title,
+            description: description.to_owned(),
+        });
+    }
+    posts_vec
 }
 
 #[cfg(test)]
 mod tests {
-    use std::fs::{read_dir, read_to_string};
-
-    use scraper::{Html, Selector};
+    use crate::get_posts;
+    use std::path::Path;
 
     #[test]
-    fn test_dir() {
-        let posts_dir = read_dir("./templates/posts").unwrap();
-        for entry in posts_dir {
-            let path = entry.unwrap().path();
-            let str = read_to_string(path).unwrap();
-            let h = Html::parse_document(&str);
-            let s = Selector::parse("title").unwrap();
-            let a = h.select(&s);
-            for v in a {
-                println!("{:?}", v.value());
-            }
-        }
+    fn test_read_metadata() {
+        let results = get_posts(Path::new("./templates/test"));
+
+        assert_eq!(results.len(), 1);
+
+        assert_eq!(results[0].title, "Test");
+        assert_eq!(results[0].description, "Cool stuff here!");
     }
 }
