24d06c1438ba937bd42790313e6bc7ae41f6f6d3
dvub <dvubdevs@gmail.com>
2024-07-30 21:28:33 -0700

    everytthing works no way

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,66 @@ use std::{
     path::Path,
 };
 
-use askama::Template;
-use axum::{extract::Path as AxumPath, routing::get, Router};
+use axum::{extract::Path as AxumPath, response::Html, routing::get, Router};
 use notify::{RecursiveMode, Watcher};
-use scraper::{Html, Selector};
+use scraper::{Html as ScraperHtml, Selector};
+
+use lazy_static::lazy_static;
+use tera::{Context, Tera};
 use tokio::net::TcpListener;
+use tower_livereload::LiveReloadLayer;
+
+lazy_static! {
+    pub static ref TERA: Tera = {
+        let mut tera = match Tera::new("templates/**/*.html") {
+            Ok(t) => t,
+            Err(e) => {
+                println!("Parsing error(s): {}", e);
+                ::std::process::exit(1);
+            }
+        };
+
+        // i dont know what this does
+        tera.autoescape_on(vec![".html"]);
+
+        tera
+    };
+}
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/posts", get(posts))
+        .route("/posts/:post", get(render_post))
+        // serve assets directory for compiled tailwind CSS
+        .nest_service("/assets", tower_http::services::ServeDir::new("assets"))
+        .layer(configure_live_reload());
+
+    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn render_post(AxumPath(r): AxumPath<String>) -> Html<String> {
+    let context = Context::new();
+    Html(TERA.render(&format!("posts/{}", r), &context).unwrap())
+}
+
+async fn root() -> Html<String> {
+    let context = Context::new();
+    Html(TERA.render("index.html", &context).unwrap())
+}
+
+async fn posts() -> Html<String> {
+    let mut context = Context::new();
+    context.insert("posts", &get_posts(Path::new("templates/posts")));
+
+    Html(TERA.render("posts.html", &context).unwrap())
+}
+
+fn configure_live_reload() -> LiveReloadLayer {
     let livereload = tower_livereload::LiveReloadLayer::new();
     let reloader = livereload.reloader();
     let mut watcher = notify::recommended_watcher(move |_| reloader.reload()).unwrap();
@@ -22,52 +72,13 @@ async fn main() {
     watcher
         .watch(Path::new("templates"), RecursiveMode::Recursive)
         .unwrap();
-
-    let app = Router::new()
-        .route("/", get(root))
-        .route("/posts", get(posts))
-        .route("/posts/:post", get(render_post))
-        // serve assets directory for compiled tailwind CSS
-        .nest_service("/assets", tower_http::services::ServeDir::new("assets"))
-        .layer(livereload);
-
-    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    livereload
 }
-
-#[derive(Template)]
-#[template(path = "{{posts}}")]
-struct PostTemplate {
-    post: String,
-}
-
-async fn render_post(AxumPath(post): AxumPath<String>) -> PostTemplate {
-    PostTemplate { post }
-}
-
-#[derive(Template)]
-#[template(path = "index.html")]
-struct HomeTemplate {}
-
-#[derive(Template)]
-#[template(path = "posts.html")]
-struct PostsTemplate {
-    posts: Vec<Post>,
-}
-
+#[derive(serde::Serialize)]
 struct Post {
+    href: String,
     title: String,
     description: String,
-}
-
-async fn root() -> HomeTemplate {
-    HomeTemplate {}
-}
-
-async fn posts() -> PostsTemplate {
-    let posts = get_posts(Path::new("./templates/posts"));
-
-    PostsTemplate { posts }
 }
 
 fn get_posts(path: &Path) -> Vec<Post> {
@@ -77,9 +88,10 @@ fn get_posts(path: &Path) -> Vec<Post> {
 
     for entry in posts_dir {
         let path = entry.unwrap().path();
-        let str = read_to_string(path).unwrap();
+        let href = &path.file_name().unwrap().to_str().unwrap();
+        let str = read_to_string(&path).unwrap();
 
-        let html = Html::parse_fragment(&str);
+        let html = ScraperHtml::parse_fragment(&str);
 
         let title_selector = Selector::parse("title").unwrap();
         let meta_selector = Selector::parse("meta").unwrap();
@@ -97,6 +109,7 @@ fn get_posts(path: &Path) -> Vec<Post> {
             }
         }
         posts_vec.push(Post {
+            href: format!("posts/{}", href.to_string()),
             title,
             description: description.to_owned(),
         });
